d0531f0e2ebeb22b4f5fb39f48ce8588dbd1eb0d
AJ <ajspeller@gmail.com>
2018-04-24 21:24:35 -0400

    Refactor TitleCase

--- src/app/title-case.pipe.ts
+++ src/app/title-case.pipe.ts
@@ -6,36 +6,24 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TitleCasePipe implements PipeTransform {
 
   transform(value: string, args?: any): any {
-    if (!value || value.length === 0 || value.trim() === '') {
+    if (!value) {
       return null;
     }
 
-    const preps = ['of', 'the', 'and'];
+    const preps = ['of', 'the', 'and', 'as', 'at', 'for'];
 
     const words = value.toLowerCase().split(' ');
-    const adjusted = words.map((word, idx) => {
+    const adjustedSentence = words.map((word, idx) => {
       const letters = word.split('');
-      if (letters.length === 0) {
-        return '';
+      if (idx >= 1 && preps.some(prep => prep === word)) {
+        return word;
       }
-      if (idx >= 1) {
-        if (word.toLowerCase() === 'of') {
-          return word;
-        }
-        if (word.toLowerCase() === 'the') {
-          return word;
-        }
-        if (word.toLowerCase() === 'and') {
-          return word;
-        }
-      }
-
 
       letters[0] = letters[0].toUpperCase();
       const adjustedWord = letters.join('');
       return adjustedWord;
     });
-    return adjusted.join(' ');
+    return adjustedSentence.join(' ');
   }
 
 }
